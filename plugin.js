e7e9e16ee40ec966d408c75ebce0658c542ace21
Petri Savolainen <64672+petri@users.noreply.github.com>
2026-01-12 15:55:05 +0200

    Rename toolbar button from 'Slides' to 'Contents'

--- plugin.js
+++ plugin.js
@@ -664,7 +664,7 @@ const Plugin = () => {
         return button;
       }
 
-      addToolbarButton('Slides', 'Slides', 'fa-images', 'fas', openPanel, true);
+      addToolbarButton('Contents', 'Slides', 'fa-images', 'fas', openPanel, true);
 
       if (options.custom) {
         options.custom.forEach(function (element, index, array) {
